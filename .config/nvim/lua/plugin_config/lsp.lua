3908d9dd1bc2e8972d798c38dea8a859adea7805
Toni Takala <toni.takala@rantalainen.fi>
2025-09-23 09:06:36 +0300

    update plugins

--- .config/nvim/lua/plugin_config/lsp.lua
+++ .config/nvim/lua/plugin_config/lsp.lua
@@ -1,9 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({})
-
-require("typescript-tools").setup({
-
-})
+require("typescript-tools").setup({})
 
 -- Set up lspconfig.
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -12,58 +9,58 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 --   capabilities = capabilities,
 -- })
 
-require("lspconfig")["lua_ls"].setup({
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      }
-    }
-  }
+vim.lsp.config("lua_ls", {
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
-require("lspconfig")["bicep"].setup({
-  capabilities = capabilities,
-  filetypes = { "bicep", "bicepparam" },
+vim.lsp.config("bicep", {
+	capabilities = capabilities,
+	filetypes = { "bicep", "bicepparam" },
 })
 
-require("lspconfig")["marksman"].setup({
-  capabilities = capabilities,
+vim.lsp.config("marksman", {
+	capabilities = capabilities,
 })
 
-require("lspconfig")["yamlls"].setup({
-  capabilities = capabilities,
+vim.lsp.config("yamlls", {
+	capabilities = capabilities,
 })
 
-require("lspconfig")["jsonls"].setup({
-  capabilities = capabilities,
+vim.lsp.config("jsonls", {
+	capabilities = capabilities,
 })
 
-require("lspconfig")["html"].setup({
-  capabilities = capabilities,
+vim.lsp.config("html", {
+	capabilities = capabilities,
 })
 
-require("lspconfig")["cssls"].setup({
-  capabilities = capabilities,
+vim.lsp.config("cssls", {
+	capabilities = capabilities,
 })
 
-require("lspconfig")["angularls"].setup({
-  capabilities = capabilities,
+vim.lsp.config("angularls", {
+	capabilities = capabilities,
 })
 
 local pse_bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services"
 local pse_command_fmt =
-[[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
+	[[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
 local pse_temp_path = vim.fn.stdpath("cache")
 local pse_command = pse_command_fmt:format(pse_bundle_path, pse_bundle_path, pse_temp_path, pse_temp_path)
 
-require("lspconfig")["powershell_es"].setup({
-  filetypes = { "ps1" },
-  bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
-  settings = { powershell = { codeFormatting = { Preset = 'OTBS' } } },
-  cmd = { "pwsh", "-NoLogo", "-Command", pse_command },
-  -- capabilities = capabilities,
+vim.lsp.config("powershell_es", {
+	filetypes = { "ps1" },
+	bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+	settings = { powershell = { codeFormatting = { Preset = "OTBS" } } },
+	cmd = { "pwsh", "-NoLogo", "-Command", pse_command },
+	-- capabilities = capabilities,
 })
 
 -- Global mappings.
@@ -76,49 +73,57 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
-    --  you are using nvim-cmp do not use neovim's built-in omnifunc as it cannot support the additional completion items returned from servers due to the capabilities enabled by nvim-cmp
-    -- vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		-- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+		--  you are using nvim-cmp do not use neovim's built-in omnifunc as it cannot support the additional completion items returned from servers due to the capabilities enabled by nvim-cmp
+		-- vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-    -- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename,
-      vim.tbl_extend("force", opts, { desc = "[LSP] [R]ename Variable" }))
-    vim.keymap.set({ "n", "v" }, "<space>lc", vim.lsp.buf.code_action,
-      vim.tbl_extend("force", opts, { desc = "[LSP] [C]ode Action" }))
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    -- vim.keymap.set('n', '<space>f', function()
-    --  vim.lsp.buf.format { async = true }
-    -- end, opts)
-    vim.keymap.set("n", "<space>lo", ":TSToolsOrganizeImports<CR>", { desc = "[LSP] [O]rganize Imports" })
-    vim.keymap.set("n", "<space>li", ":TSToolsAddMissingImports<CR>", { desc = "[LSP] Add Missing [I]mports" })
-    vim.keymap.set("n", "<space>lR", ":TSToolsRenameFile<CR>", { desc = "[LSP] [R]ename File" })
-  end,
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		-- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set(
+			"n",
+			"<space>lr",
+			vim.lsp.buf.rename,
+			vim.tbl_extend("force", opts, { desc = "[LSP] [R]ename Variable" })
+		)
+		vim.keymap.set(
+			{ "n", "v" },
+			"<space>lc",
+			vim.lsp.buf.code_action,
+			vim.tbl_extend("force", opts, { desc = "[LSP] [C]ode Action" })
+		)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		-- vim.keymap.set('n', '<space>f', function()
+		--  vim.lsp.buf.format { async = true }
+		-- end, opts)
+		vim.keymap.set("n", "<space>lo", ":TSToolsOrganizeImports<CR>", { desc = "[LSP] [O]rganize Imports" })
+		vim.keymap.set("n", "<space>li", ":TSToolsAddMissingImports<CR>", { desc = "[LSP] Add Missing [I]mports" })
+		vim.keymap.set("n", "<space>lR", ":TSToolsRenameFile<CR>", { desc = "[LSP] [R]ename File" })
+	end,
 })
 
 vim.diagnostic.config({
-  signs = true,
-  underline = true,
-  severity_sort = true,
-  virtual_text = {
-    prefix = "",
-    spacing = 4,
-  },
-  update_in_insert = false,
-  severity = {
-    error = "",
-    warning = "",
-    information = "",
-    hint = "",
-  }
+	signs = true,
+	underline = true,
+	severity_sort = true,
+	virtual_text = {
+		prefix = "",
+		spacing = 4,
+	},
+	update_in_insert = false,
+	severity = {
+		error = "",
+		warning = "",
+		information = "",
+		hint = "",
+	},
 })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,224 +1,235 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
 local plugins = {
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
-  { "nvim-tree/nvim-web-devicons",              lazy = true },
-  -- { "nvim-tree/nvim-tree.lua" },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	-- { "nvim-tree/nvim-tree.lua" },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
-  {
-    "kkoomen/vim-doge",
-    config = function()
-    end
-  },
+	{
+		"kkoomen/vim-doge",
+		config = function() end,
+	},
 
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "BufWinEnter",
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "vscode",
-          icons_enabled = true,
-        },
-        sections = {
-          lualine_a = {
-            {
-              "filename",
-              path = 1,
-            },
-          },
-        },
-      })
-    end,
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "BufWinEnter",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "vscode",
+					icons_enabled = true,
+				},
+				sections = {
+					lualine_a = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
+				},
+			})
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			-- 1) Make Neovim recognize *.bicepparam as filetype=bicepparam
+			vim.filetype.add({
+				extension = {
+					bicepparam = "bicepparam",
+				},
+			})
 
-      configs.setup({
-        ensure_installed = { "lua", "javascript", "html", "css", "json", "typescript", "bicep" },
+			-- 2) Reuse the bicep Treesitter grammar for bicepparam
+			pcall(function()
+				vim.treesitter.language.register("bicep", "bicepparam")
+			end)
 
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-        }
-      })
-    end,
-  },
+			local configs = require("nvim-treesitter.configs")
 
-  {
-    "nvim-treesitter/nvim-treesitter-context"
-  },
+			configs.setup({
+				ensure_installed = { "lua", "javascript", "html", "css", "json", "typescript", "bicep" },
 
-  {
-    "github/copilot.vim",
-    event = "BufWinEnter",
-  },
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+				-- Turn on treesitter-based indentation (python exception just there for future reference)
+				indent = { enable = true, disable = { "python" } },
+			})
+		end,
+	},
 
-  {
-    "dstein64/vim-startuptime",
-    -- lazy-load on a command
-    cmd = "StartupTime",
-    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
-    init = function()
-      vim.g.startuptime_tries = 5
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
 
-  { -- Install ripgrep to optimize telescope: https://github.com/BurntSushi/ripgrep
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-  },
+	{
+		"github/copilot.vim",
+		event = "BufWinEnter",
+	},
 
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	{
+		"dstein64/vim-startuptime",
+		-- lazy-load on a command
+		cmd = "StartupTime",
+		-- init is called during startup. Configuration for vim plugins typically should be set in an init function
+		init = function()
+			vim.g.startuptime_tries = 5
+		end,
+	},
 
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-  },
+	{ -- Install ripgrep to optimize telescope: https://github.com/BurntSushi/ripgrep
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+	},
 
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+	},
 
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "BufWinEnter",
-    config = function()
-      require("gitsigns").setup({
-        current_line_blame = true,
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
-          ignore_whitespace = false,
-          virt_text_priority = 100,
-        },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-      })
-    end,
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufWinEnter",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+				},
+			})
+		end,
+	},
 
-  {
-    "stevearc/conform.nvim",
-  },
+	{
+		"stevearc/conform.nvim",
+	},
 
-  {
-    "williamboman/mason.nvim",
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+	},
 
-  {
-    "hrsh7th/cmp-nvim-lsp",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/vim-vsnip",
-      "hrsh7th/cmp-nvim-lsp-signature-help"
-    },
-  },
+	{
+		"hrsh7th/cmp-nvim-lsp",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+		},
+	},
 
-  {
-    "numToStr/Comment.nvim",
-    opts = {
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		lazy = false,
+	},
 
-    },
-    lazy = false
-  },
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("dressing").setup()
+		end,
+	},
 
-  {
-    "stevearc/dressing.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("dressing").setup()
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 500
 
-  {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
+			local wk = require("which-key")
+			wk.setup()
+		end,
+	},
 
-      local wk = require("which-key")
-      wk.setup();
-    end,
-  },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
 
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-  },
-
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -227,38 +238,36 @@ local plugins = {
       -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
-  },
-  {
-    'echasnovski/mini.surround',
-    version = false,
-    opts = {
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		opts = {
 
-      mappings = {
-        add = 'ma',          -- Add surrounding in Normal and Visual modes
-        delete = 'md',       -- Delete surrounding
-        find = '',           -- Find surrounding (to the right)
-        find_left = '',      -- Find surrounding (to the left)
-        highlight = 'mh',    -- Highlight surrounding
-        replace = 'mr',      -- Replace surrounding
-        update_n_lines = '', -- Update `n_lines`
+			mappings = {
+				add = "ma", -- Add surrounding in Normal and Visual modes
+				delete = "md", -- Delete surrounding
+				find = "", -- Find surrounding (to the right)
+				find_left = "", -- Find surrounding (to the left)
+				highlight = "mh", -- Highlight surrounding
+				replace = "mr", -- Replace surrounding
+				update_n_lines = "", -- Update `n_lines`
 
-        suffix_last = '',    -- Suffix to search with "prev" method
-        suffix_next = '',    -- Suffix to search with "next" method
-      },
+				suffix_last = "", -- Suffix to search with "prev" method
+				suffix_next = "", -- Suffix to search with "next" method
+			},
+		},
+	},
 
-    }
-  },
-
-  -- {
-  --   'norcalli/nvim-colorizer.lua',
-  --   config = function()
-  --     require('colorizer').setup({
-  --       '*', -- Highlight all files, but customize some others.
-  --       css = { rgb_fn = true }
-  --     })
-  --   end
-  -- }
-
+	-- {
+	--   'norcalli/nvim-colorizer.lua',
+	--   config = function()
+	--     require('colorizer').setup({
+	--       '*', -- Highlight all files, but customize some others.
+	--       css = { rgb_fn = true }
+	--     })
+	--   end
+	-- }
 }
 
 require("lazy").setup(plugins)
